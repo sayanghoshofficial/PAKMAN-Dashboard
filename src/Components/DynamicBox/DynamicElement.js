40f927aaa648cd250d71aeb62b0f33203ccfbdb3
Sayan Ghosh <sayanghoshofficial@users.noreply.github.com>
2023-11-08 19:37:54 +0530

    Create DynamicElementData set

--- src/Components/DynamicBox/DynamicElement.js
+++ src/Components/DynamicBox/DynamicElement.js
@@ -1,73 +1,25 @@
 import React, { useState } from 'react';
 import Style from './dynamicbox.module.css';
 import InnerHeader from './InnerHeader/InnerHeader';
+import { DynamicElementClassData } from '../../Data';
 
 const DynamicElement = () => {
     const divArr = [1, 2, 3, 4, 5, 6];
-    const [classes, setClasses] = useState({
-        0: Style.levelOne,
-        1: Style.levelTwo,
-        2: Style.levelThree,
-        3: Style.levelThree,
-        4: Style.levelThree,
-        5: Style.levelThree,
-    });
+    const [classes, setClasses] = useState(DynamicElementClassData[0]);
 
     const handleClick = (index) => {
         if (index === 1) {
-            setClasses({
-                0: Style.levelOne,
-                1: Style.levelTwo,
-                2: Style.levelThree,
-                3: Style.levelThree,
-                4: Style.levelThree,
-                5: Style.levelThree,
-            })
+            setClasses(DynamicElementClassData[0])
         } else if (index === 2) {
-            setClasses({
-                0: Style.levelTwo,
-                1: Style.levelOne,
-                2: Style.levelThree,
-                3: Style.levelThree,
-                4: Style.levelThree,
-                5: Style.levelThree,
-            })
+            setClasses(DynamicElementClassData[1])
         } else if (index === 3) {
-            setClasses({
-                0: Style.levelThree,
-                1: Style.levelThree,
-                2: Style.levelOne,
-                3: Style.levelTwo,
-                4: Style.levelThree,
-                5: Style.levelThree,
-            })
+            setClasses(DynamicElementClassData[2])
         } else if (index === 4) {
-            setClasses({
-                0: Style.levelThree,
-                1: Style.levelThree,
-                2: Style.levelTwo,
-                3: Style.levelOne,
-                4: Style.levelThree,
-                5: Style.levelThree,
-            })
+            setClasses(DynamicElementClassData[3])
         } else if (index === 5) {
-            setClasses({
-                0: Style.levelThree,
-                1: Style.levelThree,
-                2: Style.levelThree,
-                3: Style.levelThree,
-                4: Style.levelOne,
-                5: Style.levelTwo,
-            })
+            setClasses(DynamicElementClassData[4])
         } else if (index === 6) {
-            setClasses({
-                0: Style.levelThree,
-                1: Style.levelThree,
-                2: Style.levelThree,
-                3: Style.levelThree,
-                4: Style.levelTwo,
-                5: Style.levelOne,
-            })
+            setClasses(DynamicElementClassData[5])
         }
     };
 
---
+++ src/Data/index.jsx
@@ -0,0 +1,7 @@
+import DynamicElementClassData from "./DynamicElementClassData";
+
+
+
+
+
+export { DynamicElementClassData };
