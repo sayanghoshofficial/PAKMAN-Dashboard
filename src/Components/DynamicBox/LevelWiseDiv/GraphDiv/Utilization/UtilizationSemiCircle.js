2c25cdbb10732433449819418c83737953e940fa
Sayan Ghosh <sayanghoshofficial@users.noreply.github.com>
2023-11-14 18:25:35 +0530

    add hand to utilization progress bar

--- src/Components/DynamicBox/LevelWiseDiv/GraphDiv/Utilization/UtilizationSemiCircle.js
+++ src/Components/DynamicBox/LevelWiseDiv/GraphDiv/Utilization/UtilizationSemiCircle.js
@@ -4,7 +4,8 @@ import Style from './utilization.module.css';
 const UtilizationSemiCircle = ({ value }) => {
   return (
     <div className={Style.outerDiv}>
-      <div className={Style.semiCircle} style={{ '--percentage': value }}> 
+      <div className={Style.semiCircle} style={{ '--percentage': value }}>
+        <div className={Style.minuteHand}></div>
       </div>
     </div>
   )
