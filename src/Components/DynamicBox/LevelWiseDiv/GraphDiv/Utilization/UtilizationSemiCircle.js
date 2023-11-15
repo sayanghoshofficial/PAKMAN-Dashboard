f1e031554c7ff9891f34ed1eb7a59887865c9e4c
Sayan Ghosh <sayanghoshofficial@users.noreply.github.com>
2023-11-15 18:03:45 +0530

    set minitute hand and add title for Utilization semicircle

--- src/Components/DynamicBox/LevelWiseDiv/GraphDiv/Utilization/UtilizationSemiCircle.js
+++ src/Components/DynamicBox/LevelWiseDiv/GraphDiv/Utilization/UtilizationSemiCircle.js
@@ -4,14 +4,15 @@ import Style from './utilization.module.css';
 const UtilizationSemiCircle = ({ elem }) => {
   return (
     <div className={Style.outerDiv}>
-      <div className={Style.semiCircle} style={{ '--percentage': elem?.value }}>
-        <div className={Style.minuteHand}></div>
-        <div className={Style.hoverElement}>
-        {`${elem?.unit} - ${elem?.value} %`}
-        </div>
+      <div 
+      className={Style.semiCircle} 
+      style={{ '--percentage': elem?.value }} 
+      title={`${elem?.unit} - ${elem?.value} %`}
+      >
+      <div className={Style.minuteHand}></div>
       </div>
     </div>
-  )
+  );
 }
 
 export default UtilizationSemiCircle;
